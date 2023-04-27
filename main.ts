cdd0d4369ceaebb3419b1c3c1331970b5804fcd6
milly12-microbit <132004561+milly12-microbit@users.noreply.github.com>
2023-04-27 19:53:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.showString("HALLOUNDHERZLICHWILLKOMMENZUMAREIKESSTICK! (:")
+music.playMelody("C D E D E F E F ", 120)
+music.playMelody("G F G A G A B C5 ", 120)
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+})
